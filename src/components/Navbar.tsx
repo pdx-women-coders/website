f08793337f3e2d7d359bdc448fb8746a66189ce9
Brittany Ellich <brittanyellich@github.com>
2024-06-07 20:53:47 -0700

    Setup navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom"
+import '../App.css';
 
 const Navbar =()=>{
       return (
-            <div>
-                  <Link to="/">Home</Link>
-                  <Link to="/about">About</Link>
-                  <Link to="https://www.meetup.com/pdx-women-coders/" target="_blank" rel="noopener noreferrer">Meetups</Link>
-                  <Link to="/contact">Contact</Link>
-            </div>
+            <nav className="Navbar">
+                  <div className="Navbar-logo" />
+                  <div className="Navbar-links">
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="https://www.meetup.com/pdx-women-coders/" target="_blank" rel="noopener noreferrer">Meetups</Link>
+                        <Link to="/contact">Contact</Link>
+                  </div>
+                  <div className="Navbar-cta" />
+            </nav>
       )
 }
 export default Navbar;
