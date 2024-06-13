950cc260ccce83ea1acd37a02804f30f8acb197e
Brittany Ellich <brittanyellich@github.com>
2024-06-12 21:54:49 -0700

    Add some basic content to site

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,7 +1,10 @@
 const About =()=>{
     return (
-          <div>
-            About Under Construction!
+          <div className="About">
+            <h1>About Us</h1>
+            <p>PDX Women Coders is an organization dedicated to supporting and empowering women in the technology field in the Portland area throughout their career. We provide a welcoming community where women can network, collaborate, and grow their skills through workshops, networking opportunities, and community events.</p>
+            <h2>Our Mission</h2>
+            <p>Our mission is to foster an inclusive environment that encourages innovation, professional development, and mutual support, helping women thrive in their tech careers.</p>
           </div>
     )
 }
--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -2,9 +2,14 @@ import Button from "../components/Button";
 
 const Contact =()=>{
     return (
-          <div>
-            Contact Under Construction!
-            <Button onClick={()=>{console.log("Clicked!")}} text="Click me!" />
+          <div className="Contact">
+            <h1>Get in touch</h1>
+            <p>We'd love to hear from you! Whether you have questions about our events, want to get involved, or just want to say hello, feel free to reach out to us.</p>
+            
+            <Button onClick={(e) => {
+                window.location.href = 'mailto:pdxwomencoders@gmail.com';
+                e.preventDefault();
+              }} text="Email us" />
           </div>
     )
 }
